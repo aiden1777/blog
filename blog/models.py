f708da1f21d01132c92c7c91e6d8aebfaef82bd6
aiden <aiden.jung@keysupreme.com>
2018-06-29 14:15:35 +0900

    initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # tags = models.CharField(max_length=100, blank=True)
+    # lnglat = models.CharField(max_length=50, blank=True, help_text='위도/경도 포맷으로 작성할것')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
+    # def __str__(self):
+    #     return self.title
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-id']
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,55 @@
 # blog.views.py
-
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, resolve_url, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post, Comment
 # Create your views here.
-def index(request):
-    return render(request, 'blog/index.html')
+index = ListView.as_view(model=Post, template_name='blog/index.html')
+post_new = CreateView.as_view(model=Post, fields='__all__')
+post_detail = DetailView.as_view(model=Post)
+post_edit = UpdateView.as_view(model=Post, fields = '__all__')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+
+post_delete = PostDeleteView.as_view()
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    fields = ['message']
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+comment_new = CommentCreateView.as_view()
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    fields = ['message']
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+comment_edit = CommentUpdateView.as_view()
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+comment_delete = CommentDeleteView.as_view()
+
+
+
+# comment_delete = DeleteView.as_view(model=Comment)
+
+# post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:index'))
